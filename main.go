eed6e04bf4147d55988fdf64678d713e123e7d58
albanybuipe96 <mohammedarahman96@gmail.com>
2024-07-03 22:51:50 +0000

    implemented delete recipe endpoint

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func UpdateRecipeHandler(c *gin.Context) {
 	}
 
 	if index == -1 {
-		c.JSON(http.StatusBadRequest, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"error": "Recipe not found",
 		})
 		return
@@ -80,10 +80,34 @@ func UpdateRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
+func DeleteRecipeHandler(c *gin.Context) {
+	log.Println("Deleted hitted")
+	id := c.Param("id")
+	index := -1
+	for i := 0; i < len(recipes); i += 1 {
+		if recipes[i].ID == id {
+			index = 1
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index + 1:]...)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe deleted successfully",	
+	})
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
 	router.Run()
 }
